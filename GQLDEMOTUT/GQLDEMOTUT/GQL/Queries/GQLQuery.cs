971ad72cd2ab484c2fde166b8098a39eb327e180
Ahmed Samir Abd El AAL <aabdelaal@sure.com.sa>
2022-07-18 17:57:47 +0200

    sec session pagination

--- GQLDEMOTUT/GQLDEMOTUT/GQL/Queries/GQLQuery.cs
+++ GQLDEMOTUT/GQLDEMOTUT/GQL/Queries/GQLQuery.cs
@@ -15,4 +15,15 @@ public partial class GQLQuery
         return _ctox.GQLUsers;
     }
 
+    [UseDbContext(typeof(AppDbContext))]
+    // allow to query a child object
+    [UsePaging(MaxPageSize = 1000, IncludeTotalCount = true, DefaultPageSize = 10)]
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public IQueryable<Post> GetPosts([ScopedService] AppDbContext _ctox)
+    {
+        return _ctox.Posts;
+    }
+
 }
